eead9e7e878a940a76eba5ba5653f837f380d68e
simodf7 <simonedifraia7@gmail.com>
2026-07-25 09:43:18 +0200

    Restoring default startup address in test makefile

--- tests/kernel/common.mk
+++ tests/kernel/common.mk
@@ -6,6 +6,8 @@ else
 CFLAGS += -march=rv32imaf_zbkb_zbkc_zbkx_zknd_zkne_zknh -mabi=ilp32f
 endif
 
+STARTUP_ADDR ?= 0x80000000
+
 #Adding CFLAGS for STARTUP_ADDR if redefined
 ifdef STARTUP_ADDR 
 CFLAGS += -DSTARTUP_ADDR=$(STARTUP_ADDR)
